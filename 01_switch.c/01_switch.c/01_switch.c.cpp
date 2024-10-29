095a9321c5d00c8c229afe0cdb4ed86caa11bfa6
pangfeng712 <1839424376@qq.com>
2024-10-29 17:49:49 +0800

    Add files via upload

---
+++ 01_switch.c/01_switch.c/01_switch.c.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main()
+{
+	int week;
+	printf("请输入星期数：");
+	scanf_s("%d", &week);
+	switch (week) 
+	{
+		case 1:
+		    printf("跑步");
+		    break;
+	    case 2:
+		    printf("游泳");
+		    break;
+		case 3:
+			printf("慢走");
+			break;
+		case 4:
+			printf("动感电车");
+			break;
+		case 5:
+			printf("拳击");
+			break;
+		case 6:
+			printf("爬山");
+			break;
+		case 7:
+			printf("好好吃一顿");
+			break;
+		default:
+			printf("没有这个星期");
+			break;
+	}
+		return 0;
+}
